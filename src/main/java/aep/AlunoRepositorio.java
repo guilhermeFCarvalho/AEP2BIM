f79441cf32013419e8b39515c3ab65930a4ac220
willgkm <wkuviatkovski@gmail.com>
2021-05-27 17:20:35 -0300

    Revert "Adicionado método insert"
    This reverts commit db265e3732ef3f14af5e31dd7c4067fdfff0639b.

--- src/main/java/aep/AlunoRepositorio.java
+++ src/main/java/aep/AlunoRepositorio.java
@@ -3,14 +3,13 @@ package aep;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 public class AlunoRepositorio {
 	private Connection conexao;
 	public AlunoRepositorio(){
-		abrirConexao();
+		abrirConexão();
 	}
-	private void abrirConexao() {
+	private void abrirConexão() {
 		try {
 			conexao = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/bd-aep","sa","");			
 			PreparedStatement psCreateTable = conexao.prepareStatement("create table if not exists aluno ("
@@ -23,19 +22,5 @@ public class AlunoRepositorio {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
-	public void inserir(Aluno novo) {
-		try {
-			PreparedStatement psInsert = conexao.prepareStatement(
-					"insert into alunos(nome, matricula) values (?, ?)");
-			psInsert.setString(1, novo.getNome());
-			psInsert.setString(1, novo.getMatricula());
-			psInsert.execute();
-			psInsert.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		
 	}
 }
